a946d95ba8b7144d8b211226142bf209dc9df368
RiverMountain <wrm244@outlook.com>
2023-04-14 00:39:53 +0800

    non js

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,17 +17,17 @@ npm install hexo-cli -g
 npm install hexo-deployer-git --save
 
 # deployment
-if [ "$INPUT_COMMIT_MSG" = "none" ]
-then
+# if [ "$INPUT_COMMIT_MSG" = "none" ]
+# then
     hexo g -d
-elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
-then
-    # pull original publish repo
-    NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d
-else
-    NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d -m "$INPUT_COMMIT_MSG"
-fi
+# elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
+# then
+#     # pull original publish repo
+#     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
+#     hexo g -d
+# else
+#     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
+#     hexo g -d -m "$INPUT_COMMIT_MSG"
+# fi
 
 echo ::set-output name=notify::"Deploy complate."
