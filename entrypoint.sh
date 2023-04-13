6d0bc4ddf6b7a402d16b5efb6ebb911a56dae33e
RiverMountain <wrm244@outlook.com>
2023-04-14 00:48:52 +0800

    同步部署历史

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,12 +19,12 @@ npm install hexo-deployer-git --save
 # deployment
 # if [ "$INPUT_COMMIT_MSG" = "none" ]
 # then
-    hexo g -d
+#     hexo g -d
 # elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 # then
-#     # pull original publish repo
-#     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-#     hexo g -d
+    # pull original publish repo
+NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
+hexo g -d
 # else
 #     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
 #     hexo g -d -m "$INPUT_COMMIT_MSG"
