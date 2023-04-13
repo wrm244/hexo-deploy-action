eb0df3bf885ce87db9f18eedebb97803d5cc16a3
RiverMountain <wrm244@outlook.com>
2023-04-14 02:15:11 +0800

    更改时区

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,16 +9,17 @@ echo "$INPUT_DEPLOY_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 
+timedatectl set-timezone Asia/Shanghai
+
 # setup deploy git account
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
-echo "install hexo env"
+echo "install hexo env..."
 # install hexo env
 npm install hexo-cli -g
-npm install
+npm ci
 npm install hexo-deployer-git --save
-npm install hexo-auto-category --save
 
 echo "deploying......"
 NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
